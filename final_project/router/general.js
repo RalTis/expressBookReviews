255376e1c413d39153d5b9151d43b72f504a09d7
RalTis <73250538+RalTis@users.noreply.github.com>
2024-04-26 13:54:00 +0200

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,7 +12,7 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-    res.send(JSON.stringify(books,null,4));
+    res.send(books);
 });
 
 // Get book details based on ISBN
